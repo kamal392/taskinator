43f32557967528e35d878c36927bbe6d156ec1cc
kamal kishore <kamal.kishore981@gmail.com>
2024-07-25 23:45:29 -0400

    replaced anonymous function with createTaskHandler and used preventDefault().

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 // console.dir display the HTML element as an object know as DOM element.
 // console.dir(window.document);
-var buttonEl = document.querySelector("button");
+// var buttonEl = document.querySelector("button");
+// Moved event listener from button to the form element.
 
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
 // Created TaskHandler function to replace anonymous function.
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
   var taskListEl = document.createElement("li");
   taskListEl.textContent = "This is the first task that got added !";
   taskListEl.className = "task-item";
   taskToDoEl.appendChild(taskListEl);
-};
+}
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+console.log(createTaskHandler);
