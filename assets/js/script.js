a70ce15c3d13826b70f6dcf459e833f05185b035
kamal kishore <kamal.kishore981@gmail.com>
2024-07-27 19:28:30 -0400

    Captured task name and task type , displayed on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,32 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 // Created TaskHandler function to replace anonymous function.
 var createTaskHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // console.log(taskTypeInput);
+  // create list item
   var taskListEl = document.createElement("li");
-  taskListEl.textContent = "This is the first task that got added !";
   taskListEl.className = "task-item";
+
+  // create div to hold task info and add to list item.
+  var taskInfoEl = document.createElement("div");
+  // give it a class.
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    " </h3><span class ='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  // once we get data we are appending it to the list.
+  taskListEl.appendChild(taskInfoEl);
+  // here we appending the entire list into the parent <ul>
   taskToDoEl.appendChild(taskListEl);
-}
+  console.dir(taskListEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
 console.log(createTaskHandler);
