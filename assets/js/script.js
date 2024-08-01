0881080f972cb4e3c61214a41ba9f77cb2a9b2b9
kamal kishore <kamal.kishore981@gmail.com>
2024-07-31 21:27:45 -0400

    dynamically created Edit button , Delete button and select option.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 // console.dir(window.document);
 // var buttonEl = document.querySelector("button");
 // Moved event listener from button to the form element.
+// creating a variable that is going to hold Id number for each task that gets created . with an initial value of 0.
+var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
@@ -42,7 +44,8 @@ var createTaskEl = function (taskDataObj) {
   // create list item
   var taskListEl = document.createElement("li");
   taskListEl.className = "task-item";
-
+  // add task id as custom attribute.
+  taskListEl.setAttribute("data-task-id", taskIdCounter);
   // create div to hold task info and add to list item.
   var taskInfoEl = document.createElement("div");
   // give it a class.
@@ -57,8 +60,65 @@ var createTaskEl = function (taskDataObj) {
 
   // once we get data we are appending it to the list.
   taskListEl.appendChild(taskInfoEl);
+  //  createTaskAction() returns a DOM element,we can store
+  //  that element in a variable taskActionsEl.
+  var taskActionsEl = createTaskActions(taskIdCounter);
+
+  // console.log(taskActionsEl); to verify if the function is working.
+  // appending taskActionsEl which has delete button , edit button and select element.
+  taskListEl.appendChild(taskActionsEl);
+
   // here we appending the entire list into the parent <ul>
   taskToDoEl.appendChild(taskListEl);
+  // Increase task counter for next Unique Id
+  taskIdCounter++;
 };
+
+// Function which is going to dynamically create form element (delete button ,edit button and select dropdown)
+var createTaskActions = function (taskId) {
+  // create div element , which will act as a container for other elements ,delete button
+  //  edit button and select.
+  var actionContainerEl = document.createElement("div");
+  actionContainerEl.className = "task-actions";
+  // create Edit button.
+  var editButtonEl = document.createElement("button");
+  editButtonEl.textContent = "Edit";
+  editButtonEl.className = "btn edit-btn";
+  editButtonEl.setAttribute("data-task-id", taskId);
+  actionContainerEl.appendChild(editButtonEl);
+
+  //  create Delete button.
+  var deleteButtonEl = document.createElement("button");
+  deleteButtonEl.textContent = "Delete";
+  deleteButtonEl.className = "btn delete-btn";
+  deleteButtonEl.setAttribute("data-task-id", taskId);
+
+  actionContainerEl.appendChild(deleteButtonEl);
+
+  // create Select dropdown
+  var statusSelectEl = document.createElement("select");
+
+  statusSelectEl.className = "select-status";
+  statusSelectEl.setAttribute("name", "status-change");
+  statusSelectEl.setAttribute("data-task-id", taskId);
+  actionContainerEl.appendChild(statusSelectEl);
+
+  // I could create option element with its value and append them to the select element
+  // but that would lead to the similar looking code . in order to dry the code i will use for loop .
+  // and also make it easier to add a more choices
+  var statusChoices = ["To Do", "In Progress", "Completed"];
+  for (var i = 0; i < statusChoices.length; i++) {
+    //create an option element
+    var statusOptionEl = document.createElement("option");
+    statusOptionEl.textContent = statusChoices[i];
+    statusOptionEl.setAttribute("value", statusChoices[i]);
+
+    // append to select
+    statusSelectEl.appendChild(statusOptionEl);
+  }
+
+  return actionContainerEl;
+};
+
 formEl.addEventListener("submit", taskFormHandler);
 console.log(taskFormHandler);
